c69127a1d7a2bf75426fda10b70221c515c4201c
Micha <blazyngtrails@yahoo.de>
2025-09-16 21:02:27 +0200

    changed into raw HTTP and JSON

--- go.mod
+++ go.mod
@@ -1,16 +1,3 @@
 module github.com/blazyng/AniListPublicSeasonalSearch
 
 go 1.25.1
-
-require (
-	github.com/Khan/genqlient v0.8.1 // indirect
-	github.com/agnivade/levenshtein v1.2.1 // indirect
-	github.com/alexflint/go-arg v1.6.0 // indirect
-	github.com/alexflint/go-scalar v1.2.0 // indirect
-	github.com/bmatcuk/doublestar/v4 v4.9.1 // indirect
-	github.com/vektah/gqlparser/v2 v2.5.30 // indirect
-	golang.org/x/mod v0.28.0 // indirect
-	golang.org/x/sync v0.17.0 // indirect
-	golang.org/x/tools v0.37.0 // indirect
-	gopkg.in/yaml.v2 v2.4.0 // indirect
-)
--- main.go
+++ main.go
@@ -1,36 +1,94 @@
 package main
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-
-	// Import the generated package
-	"github.com/blazyng/AniListPublicSeasonalSearch/generated"
 )
 
+// The Go structs to hold the JSON response from the API.
+// We use `json:"..."` tags to map the JSON keys to our struct fields.
+type ResponseData struct {
+	Data struct {
+		Page struct {
+			Media []struct {
+				ID    int `json:"id"`
+				Title struct {
+					Romaji  string `json:"romaji"`
+					English string `json:"english"`
+				} `json:"title"`
+			} `json:"media"`
+		} `json:"Page"`
+	} `json:"data"`
+}
+
+// A struct to hold our GraphQL query and variables, which we will convert to JSON.
+type GraphQLRequest struct {
+	Query     string                 `json:"query"`
+	Variables map[string]interface{} `json:"variables"`
+}
+
 func main() {
-	// Create a standard HTTP client
-	httpClient := http.DefaultClient
+	// Define the GraphQL query as a multi-line string.
+	query := `
+		query($season: MediaSeason, $seasonYear: Int) {
+			Page {
+				media(season: $season, seasonYear: $seasonYear, type: ANIME, sort: POPULARITY_DESC) {
+					id
+					title {
+						romaji
+						english
+					}
+				}
+			}
+		}`
 
-	// The generated code provides a NewClient function
-	graphqlClient := generated.NewClient(httpClient)
-
-	// Call the generated function, which is named after your query
-	response, err := generated.GetAnimeBySeason(
-		context.Background(),
-		graphqlClient,
-		"WINTER", // The value for the $season variable
-		2024,     // The value for the $seasonYear variable
-	)
-	if err != nil {
-		log.Fatal(err)
+	// Create the request payload object.
+	requestPayload := GraphQLRequest{
+		Query: query,
+		Variables: map[string]interface{}{
+			"season":     "WINTER",
+			"seasonYear": 2024,
+		},
 	}
 
-	// Now you can access the response data with type safety!
-	// No more manual JSON parsing.
-	for _, anime := range response.Page.Media {
-		fmt.Printf("- %s (%s)\n", anime.Title.Romaji, anime.Title.English)
+	// Convert the payload object to a JSON byte buffer.
+	payloadBytes, err := json.Marshal(requestPayload)
+	if err != nil {
+		log.Fatal("Error creating JSON payload:", err)
+	}
+	payloadBuffer := bytes.NewBuffer(payloadBytes)
+
+	// Create the HTTP POST request.
+	req, err := http.NewRequestWithContext(context.Background(), "POST", "https://graphql.anilist.co", payloadBuffer)
+	if err != nil {
+		log.Fatal("Error creating HTTP request:", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+
+	// Execute the request.
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		log.Fatal("Error sending request:", err)
+	}
+	defer resp.Body.Close()
+
+	// Decode the JSON response into our Go structs.
+	var responseData ResponseData
+	if err := json.NewDecoder(resp.Body).Decode(&responseData); err != nil {
+		log.Fatal("Error decoding JSON response:", err)
+	}
+
+	// Print the results.
+	fmt.Println("Successfully fetched anime for WINTER 2024:")
+	for _, anime := range responseData.Data.Page.Media {
+		if anime.Title.Romaji != "" {
+			fmt.Printf("- %s (%s)\n", anime.Title.Romaji, anime.Title.English)
+		}
 	}
 }
